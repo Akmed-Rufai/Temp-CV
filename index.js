5cb57d740ab2744f6baa73e9300513c4a5bf99b7
Akmed-Rufai <akmedr29@gmail.com>
2023-09-06 11:21:06 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const ejs = require('ejs')
+var path = require('path')
+const bodyParser = require("body-parser")
+
+const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use('/', express.static('public'))
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//routes
+
+app.get('/', (req, res)=>{
+    res.render('CV')
+})
+app.get('/skills', (req, res)=>{
+    res.render('skills')
+})
+
+app.get('/downloads', (req, res)=>{
+    res.download('./public/mohammed.pdf')
+})
+
+
+app.listen(5000, ()=>{
+    console.log('now listening on port 5000');
+})
